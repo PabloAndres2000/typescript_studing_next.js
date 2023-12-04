a89c16104b4d2b1ecf6e9de84471aec29bd38a08
Pablo Andres Torres Labraña <76272849+PabloAndres2000@users.noreply.github.com>
2023-12-04 02:14:22 -0300

    ahora si po

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/User":["static\u002Fchunks\u002Fpages\u002FUser.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FUser","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
